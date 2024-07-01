84be694aeb6c4f86a31ee8081b7b4664909b53cb
Emilio Lehoucq <50888924+emiliolehoucq@users.noreply.github.com>
2024-07-01 11:54:30 -0500

    adding scraper

---
+++ scraper.py
@@ -0,0 +1,26 @@
+# Script to test scraping with GitHub Actions
+# Emilio Lehoucq
+
+# Importing libraries
+import requests
+from datetime import datetime
+
+# URL to scrape
+url = 'https://en.wikipedia.org/wiki/Mindfulness'
+
+# Requesting the URL
+response = requests.get(url)
+
+# Get title
+title = response.text.split('<title>')[1].split('</title>')[0]
+print(title)
+
+# Get current date and time
+dt = datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+
+# Path of the file
+path = f'data/{dt}.txt'
+
+# Save the title to a file
+with open(path, 'w') as file:
+    file.write(title)
